a74c9da5faa0c2e5475d9f22b0850b15f50fd078
Jonathan Perret <jonathan@ut7.fr>
2017-05-02 12:35:46 +0200

    Add Redis store

--- cli.js
+++ cli.js
@@ -3,9 +3,13 @@ const {store, retrieve} = require('./lib/datastore');
 if(process.argv.length > 2) {
   const data = process.argv[2];
   console.log("Storing", data);
-  store(data, function() { console.log("done"); });
+  store(data, function() {
+    console.log("done");
+    process.exit(0);
+  });
 } else {
   retrieve(function (err, tweets) {
     console.log(tweets.join('\n'));
+    process.exit(0);
   });
 }
